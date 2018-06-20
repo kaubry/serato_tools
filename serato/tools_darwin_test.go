773deca9434d3cfab5e8534588fb493645e12de4
Kevin <kevin@ubry.ch>
2018-06-20 21:33:13 +0800

    updated clean for darwin (OS X)

--- serato/tools_darwin_test.go
+++ serato/tools_darwin_test.go
@@ -2,7 +2,8 @@ package serato
 
 func getTableForFilePathTest() []TestTable {
 	tables := []TestTable{
-		{"GDrive\\DJ\\2012\\Electro\\Drum n Bass\\test.mp3", "E:\\_Serato_", "E:\\GDrive\\DJ\\2012\\Electro\\Drum n Bass\\test.mp3"},
+		{"/GDrive/DJ/Hip-Hop/Latin/Test.mp3", "/Volumes/128Go SD/GDrive/_Serato_", "/Volumes/128Go SD/GDrive/DJ/Hip-Hop/Latin/Test.mp3"},
+		{"/Users/test/Desktop/Test.mp3", "/Users/kevin/Music/_Serato_", "/Users/test/Desktop/Test.mp3"},
 	}
 	return tables
 }
--- serato/tools_test.go
+++ serato/tools_test.go
@@ -11,8 +11,10 @@ type TestTable struct {
 func TestGetFilePath(t *testing.T) {
 	for _, table := range getTableForFilePathTest() {
 		s, e := GetFilePath(table.path, table.seratoDir)
-		if e != nil && s != table.result {
-			t.Errorf("File path for: %s with serato dir: %s is . Got %s", table.path, table.seratoDir, s)
+		if e != nil {
+			t.Errorf("Error: %s", e.Error())
+		} else if s != table.result {
+			t.Errorf("File path for: %s with serato dir: %s. Got %s, expected %s", table.path, table.seratoDir, s, table.result)
 		}
 	}
 }
