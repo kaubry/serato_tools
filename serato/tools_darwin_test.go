ba765e559355eb78989e0c268e402daa727a5312
Kevin Aubry <kevin@ubry.ch>
2022-08-07 23:18:44 +0800

    Fix issue with Darwin test

--- serato/tools_darwin_test.go
+++ serato/tools_darwin_test.go
@@ -1,6 +1,6 @@
 package serato
 
-var removeVolumeTestCaseExpect = []RemoveVolumeTestCase{
+var removeVolumeTestCaseExpect = []removeVolumeTestCase{
 	{"/Volumes/TestVolume1/example.mp3", "example.mp3"},
 	{"/Volumes/TestVolume1/Music/example.mp3", "Music/example.mp3"},
 	{"/Users/test/Desktop/example.mp3", "Users/test/Desktop/example.mp3"},
@@ -13,26 +13,4 @@ func getTableForFilePathTest() []TestTable {
 		{"Users/test/Desktop/Test.mp3", "/Users/kevin/Music/_Serato_", "/Users/test/Desktop/Test.mp3"},
 	}
 	return tables
-}
-
-type getDarwinVolumeTestCase struct {
-	path   string
-	result string
-}
-
-func TestGetDarwinVolume(t *testing.T) {
-	for _, test := range []getDarwinVolumeTestCase{
-		{"/Volumes/TestVolume1", "/Volumes/TestVolume1"},
-		{"/Volumes/TestVolume1/", "/Volumes/TestVolume1"},
-		{"/Volumes/Test-Volume1", "/Volumes/Test-Volume1"},
-		{"/Volumes/Test-Volume1/", "/Volumes/Test-Volume1"},
-		{"/Volumes/TestVolume1/example.mp3", "/Volumes/TestVolume1"},
-		{"/Users/test/Desktop/example.mp3", "/"},
-		{"SomeRelativePath/example.mp3", ""},
-	} {
-		result := GetDarwinVolume(test.path)
-		if result != test.result {
-			t.Errorf("expected '%s', got '%s'", test.result, result)
-		}
-	}
 }
